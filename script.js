b538d2f6c0a90983efd30ff8270d833a1f3439da
NgNatanael <natanaelxbs@yahoo.com>
2024-09-13 15:11:04 -0300

    Adiciona categorias e campo de busca

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ function redirecionarWhatsApp(item) {
     var url = "https://wa.me/" + numeroVendedora + "?text=" + encodeURIComponent(mensagem);
     window.open(url, "_blank");
 }
+
+function buscarProduto() {
+    var input = document.getElementById('searchBar').value.toLowerCase();
+    var items = document.querySelectorAll('.item');
+    
+    items.forEach(function(item) {
+        var nome = item.getAttribute('data-name').toLowerCase();
+        if (nome.includes(input)) {
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
